d4facc8097ef87967e6d797da35d12aca467692d
heritier24 <heritier.ganza@kudibooks.com>
2022-01-08 12:18:34 +0200

    added dashboard, heder and weather components * weather

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="hero" data-bg-image="assets/images/banner.png">
+     />
+      <div class="container">
+        <form action="#" class="find-location">
+          <input type="text" placeholder="Find your location..." />
+          <input type="submit" value="Find" />
+        </form>
+      </div>
+    </div>
+    
+    <Weather />
+  </div>
+</template>
+<script>
+import Weather from './Weather.vue'
+export default {
+  components: {
+    Weather,
+  }
+}
+</script>
